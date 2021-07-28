55cc3a6991373633876262994172ecf37ef0ee0f
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-28 15:32:20 +0000

    fixed the bug, was because i had the wrong version in the pom

--- hr-api-gateway-zuul/src/main/java/com/ratz/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/ratz/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
+@Controller
 public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
