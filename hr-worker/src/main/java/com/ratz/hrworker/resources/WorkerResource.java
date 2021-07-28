ff02e9e66355c3ec81db41a93591a627ffb34590
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-28 18:18:36 +0000

    actuator for runetime config refresh

--- hr-worker/src/main/java/com/ratz/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ratz/hrworker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.ratz.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ratz.hrworker.entity.Worker;
 import com.ratz.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
+	
 	@Autowired
 	private WorkerRepository repository;
+	
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
