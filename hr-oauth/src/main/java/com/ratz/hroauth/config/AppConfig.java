81c9fa11ddfd4abd870aa7b6ea0db549e9238c6a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-07-30 10:47:22 +0000

    added cors configuration

--- hr-oauth/src/main/java/com/ratz/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/ratz/hroauth/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.ratz.hroauth.config;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
